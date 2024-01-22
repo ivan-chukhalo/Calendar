4e63acca97a1b161e557cb037ef309d4b6c59082
Ivan Chukhalo <ivanchukhalo@gmail.com>
2024-01-23 00:59:37 +0200

    feat: generating month days

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const currentDate = document.querySelector(".current-date");
+const daysTag = document.querySelector(".days");
 
 let date = new Date(),
   currYear = date.getFullYear(),
@@ -19,6 +20,15 @@ const months = [
 ];
 
 const renderCalendar = () => {
+  let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  let liTag = "";
+
+  for (let i = 1; i <= lastDateOfMonth; i++) {
+    liTag += `<li>${i}</li>`;
+  }
+
   currentDate.innerText = `${months[currMonth]} ${currYear}`;
+  daysTag.innerHTML = liTag;
 };
+
 renderCalendar();

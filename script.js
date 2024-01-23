4ddbdc04819d93dbdcda77584d3b6fb089482c02
Ivan Chukhalo <ivanchukhalo@gmail.com>
2024-01-23 21:24:47 +0200

    refactor(script.js): used prettier

--- script.js
+++ script.js
@@ -23,21 +23,20 @@ const months = [
 
 const renderCalendar = () => {
   let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay(),
-      lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-      lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay(),
-      lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
+    lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+    lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay(),
+    lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
   let liTag = "";
 
-  for (let i = firstDayOfMonth - 1; i > 0; i--){
+  for (let i = firstDayOfMonth - 1; i > 0; i--) {
     liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
   }
-
 
   for (let i = 1; i <= lastDateOfMonth; i++) {
     liTag += `<li>${i}</li>`;
   }
 
-  for (let i = 1; i <= 7 - lastDayOfMonth; i++){
+  for (let i = 1; i <= 7 - lastDayOfMonth; i++) {
     liTag += `<li class="inactive">${i}</li>`;
   }
 
